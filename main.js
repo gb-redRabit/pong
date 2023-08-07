393f0aae91391cc196b346a1dda1831ef4a6a729
gb-redRabit <136236629+gb-redRabit@users.noreply.github.com>
2023-08-07 20:31:48 +0200

    1 file modified

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ window.addEventListener('resize', () => {
 
 window.onload = () => {
   canvas.addEventListener('mousedown', handlClick);
-  canvas.addEventListener('touchenter ', (e) => {
+  window.addEventListener('touchenter ', (e) => {
     console.log(e);
   });
   canvas.addEventListener('mousemove', (e) => {
